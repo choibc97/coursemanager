c668b0702a4be1d22f2b54668d6a43189d3d619a
Benjamin Choi <choi.bc97@gmail.com>
2019-12-19 09:16:34 +0000

    Delete mobile app for now and clean up website

--- CheckoutMobile/actions/types.js
+++
@@ -1,6 +0,0 @@
-// auth related stuff
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const USER_LOADING = 'USER_LOADING';
-export const USER_LOADED = 'USER_LOADED';
-export const AUTH_ERROR = 'AUTH_ERROR';
-export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
--- CheckoutMobile/components/navigators/InstructorNavigator.js
+++
@@ -1,7 +0,0 @@
-import React, {Component} from 'react';
-
-export default class InstructorNavigator extends Component {
-  render() {
-    return null;
-  }
-}
